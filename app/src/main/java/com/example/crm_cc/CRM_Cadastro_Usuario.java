7e3fc687b9e67dc738b114957272561afe583670
VirgilioUscs <134901241+VirgilioUscs@users.noreply.github.com>
2023-05-28 23:49:53 -0300

    Initial commit

---
+++ app/src/main/java/com/example/crm_cc/CRM_Cadastro_Usuario.java
@@ -0,0 +1,14 @@
+package com.example.crm_cc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CRM_Cadastro_Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crm_cadastro_usuario);
+    }
+}
